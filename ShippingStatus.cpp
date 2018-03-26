67d7ac567c6118ba1b7152e1bbd8130abe236435
Samantha Ibasitas <36210345+samanthaibasitas@users.noreply.github.com>
2018-03-26 12:03:30 -0700

    Update ShippingStatus.cpp

--- ShippingStatus.cpp
+++ ShippingStatus.cpp
@@ -1,29 +1,36 @@
-//
 //  ShippingStatus.cpp
 //  Project2
-//
 //  Created by Han, Wenlin on 2/13/18.
 //  Copyright © 2018 Han, Wenlin. All rights reserved.
-//
 
 #include "ShippingStatus.h"
+#include <time.h>
 
-ShippingStatus::ShippingStatus() {
-  //to be completed
+ShippingStatus::ShippingStatus() 
+{ //to be completed
+    shippingStatus = " "; 
+    shippingLocation = " "; 
+    shippingTime = time(NULL);
 }
 
-ShippingStatus::ShippingStatus(const string& status, const string& location, const time_t& timeUpdated) {
-  //to be completed
+ShippingStatus::ShippingStatus(const string& status, const string& location, const time_t& timeUpdated) 
+{ //to be completed
+  shippingStatus = status; 
+  shippingLocation = location; 
+  shippingTime = timeUpdated; 
 }
 
-string ShippingStatus::m_getStatus(){
-  //to be completed
+string ShippingStatus::m_getStatus()
+{//to be completed
+  return status; 
 }
 
-string ShippingStatus::m_getLocation(){
-   //to be completed
+string ShippingStatus::m_getLocation()
+{ //to be completed
+  return location; 
 }
 
-time_t ShippingStatus::m_getTime() {
-    //to be completed
+time_t ShippingStatus::m_getTime() 
+{ //to be completed
+  return timeUpdated; 
 }
