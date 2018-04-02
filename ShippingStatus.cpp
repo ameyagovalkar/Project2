4963726f1ab104bd45eb5dd442b2db7f37dbd92f
Samantha Ibasitas <36210345+samanthaibasitas@users.noreply.github.com>
2018-04-02 14:35:30 -0700

    Update ShippingStatus.cpp

--- ShippingStatus.cpp
+++ ShippingStatus.cpp
@@ -8,29 +8,29 @@
 
 ShippingStatus::ShippingStatus() 
 { //to be completed
-    status = " "; 
-    location = " "; 
-    timeStatus = time(NULL);
+    shippingStatus = " "; 
+    shippingLocation = " "; 
+    shippingTime = time(NULL);
 }
 
 ShippingStatus::ShippingStatus(const string& status, const string& location, const time_t& timeUpdated) 
 { //to be completed
-  ShippingStatus::status = status; 
-  ShippingStatus::location = location; 
-  timeStatus = timeUpdated; 
+  ShippingStatus::shippingStatus = status; 
+  ShippingStatus::shippingLocation = location; 
+  shippingTime = timeUpdated; 
 }
 
 string ShippingStatus::m_getStatus()
 {//to be completed
-  return status; 
+  return shippingStatus; 
 }
 
 string ShippingStatus::m_getLocation()
 { //to be completed
-  return location; 
+  return shippingLocation; 
 }
 
 time_t ShippingStatus::m_getTime() 
 { //to be completed
-  return timeStatus; 
+  return shippingTimeStatus; 
 }
