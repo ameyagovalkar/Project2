40750ab7d9740551b4a5800945f261f8ade231b2
17phamsteven <36425275+17phamsteven@users.noreply.github.com>
2018-04-02 14:43:14 -0700

    Update ShippingStatus.cpp

--- ShippingStatus.cpp
+++ ShippingStatus.cpp
@@ -6,31 +6,31 @@
 #include "ShippingStatus.h"
 #include <time.h>
 
-ShippingStatus::ShippingStatus() 
+ShippingStatus::ShippingStatus()
 { //to be completed
-    shippingStatus = " "; 
-    shippingLocation = " "; 
-    shippingTime = time(NULL);
+	status = " ";
+	location = " ";
+	timeStatus = time(NULL);
 }
 
-ShippingStatus::ShippingStatus(const string& status, const string& location, const time_t& timeUpdated) 
+ShippingStatus::ShippingStatus(const string& status, const string& location, const time_t& timeUpdated)
 { //to be completed
-  ShippingStatus::shippingStatus = status; 
-  ShippingStatus::shippingLocation = location; 
-  shippingTime = timeUpdated; 
+	ShippingStatus::status = status;
+	ShippingStatus::location = location;
+	timeStatus = timeUpdated;
 }
 
 string ShippingStatus::m_getStatus()
 {//to be completed
-  return shippingStatus; 
+	return status;
 }
 
 string ShippingStatus::m_getLocation()
 { //to be completed
-  return shippingLocation; 
+	return location;
 }
 
-time_t ShippingStatus::m_getTime() 
+time_t ShippingStatus::m_getTime()
 { //to be completed
-  return shippingTimeStatus; 
+	return timeStatus;
 }
