296b40c262eabf79349687f1a16a9132589ef4e0
ameyagovalkar <36211515+ameyagovalkar@users.noreply.github.com>
2018-04-05 22:12:18 -0700

    Update ShippingStatus.h

--- ShippingStatus.h
+++ ShippingStatus.h
@@ -22,7 +22,7 @@ public:
     string m_getStatus();
     string m_getLocation();
     time_t m_getTime();
-	ShippingStatus& ShippingStatus::operator=(const ShippingStatus& ss);
+	ShippingStatus& operator=(const ShippingStatus& ss);
 	bool operator==(const ShippingStatus& r) const;
 	bool operator!=(const ShippingStatus& r) const;
     
